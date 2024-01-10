9d0bd2dcb11fe3e711ef489bb971aa7f56e3ed6e
FerencFarkas22 <85436006+FerencFarkas22@users.noreply.github.com>
2024-01-10 18:33:19 +0100

    gameboard is generated

---
+++ 07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+const initialGameBorad = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+];
+const GameBoard = () => {
+    const [gameBoard, setGameBoard] = useState(initialGameBorad);
+
+    function handleSelectSquare(rowIndex, colIndex) {
+        setGameBoard((prevGamBoard) => {
+            const updatedBoard = [...prevGamBoard.map(innerArray => [...innerArray])];
+            updatedBoard[rowIndex][colIndex] = 'X'
+            return updatedBoard;
+
+        })
+    }
+
+    return (
+        <ol id='game-board'>
+            {gameBoard.map((row, rowIndex) => 
+            <li key={rowIndex}>
+
+                <ol>
+                    {row.map((playerSymbol, colIndex) =>
+                        <li key={colIndex}>
+                            <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                        </li>
+                    )}
+                </ol>
+
+            </li>)}
+        </ol>
+    )
+}
+
+export default GameBoard
