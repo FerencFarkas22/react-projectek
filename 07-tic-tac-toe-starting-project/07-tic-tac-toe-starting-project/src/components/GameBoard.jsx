fac72712695339cbef1837c14c89490beda96497
FerencFarkas22 <85436006+FerencFarkas22@users.noreply.github.com>
2024-01-18 18:53:48 +0100

    practice

--- 07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ 07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,27 +1,37 @@
-import React, { useState } from 'react'
+
 const initialGameBorad = [
     [null, null, null],
     [null, null, null],
     [null, null, null],
 ];
-const GameBoard = ({onSelectSquare, activePlayerSymbol}) => {
-    const [gameBoard, setGameBoard] = useState(initialGameBorad);
+const GameBoard = ({onSelectSquare, turns}) => {
+    let gameBoard = initialGameBorad;
 
-    function handleSelectSquare(rowIndex, colIndex) {
-        setGameBoard((prevGamBoard) => {
-            const updatedBoard = [...prevGamBoard.map(innerArray => [...innerArray])];
-            updatedBoard[rowIndex][colIndex] =  activePlayerSymbol
-            return updatedBoard;
-        })
-        onSelectSquare();
+    for(const turn of turns) {
+        const {square, player} = turn;
+        const { row, col} = square;
+
+        gameBoard[row][col] = player;
+
+    
     }
+   // const [gameBoard, setGameBoard] = useState(initialGameBorad);
+
+   // function handleSelectSquare(rowIndex, colIndex) {
+     //   setGameBoard((prevGamBoard) => {
+       //     const updatedBoard = [...prevGamBoard.map(innerArray => [...innerArray])];
+         //   updatedBoard[rowIndex][colIndex] =  activePlayerSymbol
+          //  return updatedBoard;
+       // })
+       // onSelectSquare();
+    //}
     return (
         <ol id='game-board'>
             {gameBoard.map((row, rowIndex) => 
             <li key={rowIndex}>
                 <ol>{row.map((playerSymbol, colIndex) =>
                         <li key={colIndex}>
-                            <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                            <button onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
                         </li>)}
                 </ol>
             </li>)}
